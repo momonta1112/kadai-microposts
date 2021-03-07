e5bea5294a506e54e8cfc37135a71ca925f83b45
momonta <nakao.subaka@gmail.com>
2021-03-07 14:57:44 +0000

    follow, unfollow

---
+++ app/Http/Controllers/UserFollowController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class UserFollowController extends Controller
+{
+    public function store($id)
+    {
+        \Auth::user()->follow($id);
+        
+        return back();
+    }
+    
+    public function destroy($id)
+    {
+        \Auth::user()->unfollow($id);
+        
+        return back();
+    }
+}
